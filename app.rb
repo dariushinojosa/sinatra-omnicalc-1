f3721fb7ae608545c7fb60caae063e37e7ccad50
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-22 17:28:35 +0000

    progress update

--- app.rb
+++ app.rb
@@ -28,11 +28,16 @@ get ("/square/results") do
 end
 
 get ("/square_root/results") do
-
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 0.5
+  erb(:sqrt_results)
 end
 
 get ("/random/results") do
-
+  @the_min = params.fetch("users_min").to_f
+  @the_max = params.fetch("users_max").to_f
+  @the_result = rand(@the_min..@the_max)
+  erb(:random_results)
 end
 
 get ("/payment/results") do
