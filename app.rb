0378dcb543d602306dd56b7807b477e4e548070e
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-22 17:00:46 +0000

    progress check

--- app.rb
+++ app.rb
@@ -2,8 +2,39 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square_root/new") do
+  erb(:new_sqrt_calc)
+end
+
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get ("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get ("/square_root/results") do
+
+end
+
+get ("/random/results") do
+
+end
+
+get ("/payment/results") do
+
 end
